e6959be0324f469fe7bf8adc62f651563ccde3c2
Ryan Lindsey <ryan.lindsey@uky.edu>
2018-08-03 10:04:35 -0400

    Having some trouble with the type system. Considering looking into static typing methods for Python, but I think I'm just going to move on to the next thing.

--- hash/hash_table.py
+++ hash/hash_table.py
@@ -1,5 +1,8 @@
 """
-    This structure was taken from: http://www.cs.rmit.edu.au/online/blackboard/chapter/05/documents/contribute/chapter/05/intro.html
+    I've been messing around with a couple of different classes for hashing. As of right now, I'm trying
+    to get the ChainedTable to work kind of like a dictionary but the type system in Python is very frustrating.
+
+    Putting this down for now.
 """
 import math
 import random as r
@@ -20,9 +23,27 @@ class LinearTable():
     def __init__(self):
         self.size = 10
         self.table = [None for _ in range(self.size)]
-    
+   
+    def key_to_int(self, key):
+        n = 0
+        for l in key:
+            n += ord(l)
+        n %= self.size
+        return n
+
     def search(self, item):
-        return self.table[item.key]
+        if type(item) == HashItem and type(item.key) == str:
+            indx = self.key_to_int(item.key)
+            ti = self.table[indx]
+        elif type(item) == HashItem and type(item.key) == int:
+            ti = self.table[item.key]
+        elif type(item) == str:
+            indx = self.key_to_int(item)
+            ti = self.table[indx]
+        if ti.key == item.key:
+            return ti
+        else:
+            return None
 
     def insert(self, item):
         self.table[item.key] = item
@@ -33,19 +54,52 @@ class LinearTable():
 class ChainedTable():
     def __init__(self):
         self.size = 10
-        self.last_insert = 0
         self.table = [[] for _ in range(self.size)]
 
+    def key_to_int(self, key):
+        n = 0
+        for l in key:
+            n += ord(l)
+        n %= self.size
+        return n
+
     def search(self, item):
-        return self.table[item.key]
+        print("Searching this table:", self.table)
+        if type(item) == HashItem and type(item.key) == str:
+            indx = self.key_to_int(item.key)
+            sub_table = self.table[indx]
+        elif type(item) == HashItem and type(item.key) == int:
+            sub_table = self.table[item.key]
+        elif type(item) == str:
+            indx = self.key_to_int(item)
+            sub_table = self.table[indx]
+        for i in range(len(sub_table)): 
+            print("looking in this subtable: ", sub_table)
+            if sub_table[i] != None:
+                if type(item) == str and sub_table[i].key == item:
+                    return sub_table[i]
+                elif type(item) == str and sub_table[i].key == item.key:
+                    return sub_table[i]
+        return None
 
     def insert(self, item):
-        self.last_insert += 1
-        self.last_insert %= self.size
-        if self.table[self.last_insert] != None
-            self.table[self.last_insert] = item
+        if type(item) == HashItem and type(item.key) == str:
+            indx = self.key_to_int(item.key)
+            sub_table = self.table[indx]
+        elif type(item) == HashItem and type(item.key) == int:
+            sub_table = self.table[item.key]
         else:
-            print("Collision!")
+            print("Problem inserting")
+            return
+        for i in range(len(sub_table)): 
+            if sub_table[i] != None and sub_table[i].key == item.key:
+                print("Item with that key already exists!")
+                return
+        sub_table.append(item)
 
     def delete(self, item):
-        self.table[item.key] = None
+        indx = self.key_to_int(item)
+        sub_table = self.table[indx]
+        for i in range(len(sub_table)): 
+            if sub_table[i] != None and sub_table[i].key == item.key:
+                sub_table[i] = None
--- hash/test.py
+++ hash/test.py
@@ -3,6 +3,16 @@ import random as r
 
 def run_all_tests():
     test_lt_class()
+    test_ct_class()
+
+def rand_words():
+    l = []
+    for _ in range(10):
+        w = [] 
+        for _ in range(5, r.randint(10,20)):
+            w.append(chr(r.randint(65,90)))
+        l.append("".join(w))
+    return l
 
 def test_lt_class():
     l = LinearTable()
@@ -16,4 +26,19 @@ def test_lt_class():
         assert table_item.key == i.key
         assert table_item.data == i.data
 
+def test_ct_class():
+    c = ChainedTable()
+    keys = rand_words()
+    items = []
+    for i in range(10):
+        h = HashItem(keys[i], i)
+        c.insert(h)
+        items.append(h)
+    for i in items:
+        table_item = c.search(i)
+        assert table_item.key == i.key
+        assert table_item.data == i.data
+        table_item = c.search(i.key)
+        assert table_item.key == i.key
+        assert table_item.data == i.data
 run_all_tests()
