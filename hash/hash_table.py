e12460b43af2c982808ffed52f4e3b5118525429
Ryan Lindsey <ryan.lindsey44@gmail.com>
2018-08-03 00:34:26 -0400

    Implemented a linear hash table. Should probably write a couple more tests for it then move on to a chained hash table

--- hash/hash_table.py
+++ hash/hash_table.py
@@ -2,6 +2,7 @@
     This structure was taken from: http://www.cs.rmit.edu.au/online/blackboard/chapter/05/documents/contribute/chapter/05/intro.html
 """
 import math
+import random as r
 
 class HashItem():
     def __init__(self, key, data):
@@ -15,19 +16,36 @@ class HashItem():
     def isDeleted(self):
         return self.deleted
 
-    def getKey(self):
-        return self.key
-
-    def getData(self):
-        return self.data
-
 class LinearTable():
     def __init__(self):
-        self.size = 7
-        self.table = []
+        self.size = 10
+        self.table = [None for _ in range(self.size)]
+    
+    def search(self, item):
+        return self.table[item.key]
 
-    def code(self, key):
-        return math.abs(self.hashCode(key) % self.size)
+    def insert(self, item):
+        self.table[item.key] = item
 
-    def hashCode(self, key):
-        pass
+    def delete(self, item):
+        self.table[item.key] = None
+
+class ChainedTable():
+    def __init__(self):
+        self.size = 10
+        self.last_insert = 0
+        self.table = [[] for _ in range(self.size)]
+
+    def search(self, item):
+        return self.table[item.key]
+
+    def insert(self, item):
+        self.last_insert += 1
+        self.last_insert %= self.size
+        if self.table[self.last_insert] != None
+            self.table[self.last_insert] = item
+        else:
+            print("Collision!")
+
+    def delete(self, item):
+        self.table[item.key] = None
--- hash/test.py
+++ hash/test.py
@@ -1,10 +1,19 @@
 from hash_table import *
+import random as r
 
 def run_all_tests():
-    pass
+    test_lt_class()
 
-def test():
-    pass
-
+def test_lt_class():
+    l = LinearTable()
+    items = []
+    for i in range(10):
+        h = HashItem(i, r.randint(1337, 6969))
+        l.insert(h)
+        items.append(h)
+    for i in items:
+        table_item = l.search(i)
+        assert table_item.key == i.key
+        assert table_item.data == i.data
 
 run_all_tests()
